769a3d2119da55f63ef85a12c0cb5566f9b5a2bb
Odilon Julio dos Santos <odilonjulio@A204-01L.imd.local>
2019-04-08 16:39:38 -0300

    já na questão 5

--- scriptTrabalhoI.R
+++ scriptTrabalhoI.R
@@ -21,19 +21,20 @@ ggplot(data2, aes(x = Sequencing.Center, y = n)) + geom_point()
 
 ##3
 
-lenGenome <- select(data, Genome.Name...Sample.Name, Genome.Size.....assembled)
+lenGenome <- select(data, Genome.Size.....assembled)
+par(mfrow = c(1, 2)) # Possibilita visualizar vários plots ao mesmo tempo.
+normal <- rnorm(1000, 0, 1) # Criando uma distribuição normal para servir como referência.
 lenGenomeSort <- arrange(lenGenome, desc(Genome.Size.....assembled))
-plot(lenGenomeSort)
-plot(lenGenome)
+qqnorm(lenGenomeSort$Genome.Size.....assembled) ## Constata se segue uma dist normal.
+qqline(lenGenomeSort$Genome.Size.....assembled, col = 'red')
+qqnorm(normal)
+qqline(normal, col = 'blue')
 
-ggplot(lenGenomeSort,
-       mapping = aes(x = Genome.Name...Sample.Name,
-                     y = Genome.Size.....assembled)) + geom_point()
-normal <- dnorm(y,
-      mean = mean(y),
-      sd = sd(y),
-      log = FALSE)
 
+##4
+countGenus <- count(data, Genus, sort=TRUE)
+View(countGenus)
+head(countGenus)
 
 
 
