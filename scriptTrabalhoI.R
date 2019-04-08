8597accb220b00d0a807fca9f9cea3be5b4ce64d
OdilonJulio <odilonjulio@ufrn.edu.br>
2019-04-08 08:03:31 -0300

    exercício 3 em andamento

--- scriptTrabalhoI.R
+++ scriptTrabalhoI.R
@@ -1,13 +1,49 @@
+## 1
+data <- read.delim("~/IMD0601/trabalhoI/data.tab");View(data)
 dim(data)
 names(data)
 tail(data)
-data <- read.delim("~/IMD0601/trabalhoI/data.tab");View(data)
-select()
 
 
+##2
+install.packages('tidyr')
+install.packages('dplyr')
+install.packages('ggplot2')
 library(ggplot2)
-data2 <- select()
-ggplot(data, aes(x = "Sequencing Center", y = "taxon_oid")) + geom_point() + facet_grid(gear~) + stat_smooth()
+library(tidyr)
+library(dplyr)
+count(data, Sequencing.Center, sort=TRUE)
+count_data <- count(data, Sequencing.Center, sort=TRUE)
+ggplot(count_data, aes(x = Sequencing.Center, y = n)) + geom_point()
+data2 <- filter(count_data, n > 150)
+ggplot(data2, aes(x = Sequencing.Center, y = n)) + geom_point()
+
+
+##3
+
+lenGenome <- select(data, Genome.Name...Sample.Name, Genome.Size.....assembled)
+lenGenomeSort <- arrange(lenGenome, desc(Genome.Size.....assembled))
+plot(lenGenomeSort)
+plot(lenGenome)
+
+ggplot(lenGenomeSort,
+       mapping = aes(x = Genome.Name...Sample.Name,
+                     y = Genome.Size.....assembled)) + geom_point()
+normal <- dnorm(y,
+      mean = mean(y),
+      sd = sd(y),
+      log = FALSE)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
